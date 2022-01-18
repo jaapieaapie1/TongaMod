e13a955c680c6f6c12589abbe31756d50de20eea
jaap <jaapelst@gmail.com>
2022-01-18 11:22:04 +0100

    init

---
+++ src/main/java/nl/marijnenjaapproducties/tongamod/TongaMod.java
@@ -0,0 +1,10 @@
+package nl.marijnenjaapproducties.tongamod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TongaMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
